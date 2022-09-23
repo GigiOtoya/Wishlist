2da447e01395b083330ae58c4fe96db09954f841
Luigi Otoya <Luigi.Otoya@gmail.com>
2022-09-22 22:49:43 -0400

    funtionality completed

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.wishlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +17,31 @@ class MainActivity : AppCompatActivity() {
 
         // Lookup the RecyclerView in activity layout
         val wishlistRv = findViewById<RecyclerView>(R.id.wishlistRv)
+        wishlistRv.layoutManager = LinearLayoutManager(this)
         // Fetch list of wishlists
-
+        val wishlistAdapter = WishlistAdapter(mutableListOf())
+        // Attach the adapter to the RecyclerView to populate items
+        wishlistRv.adapter = wishlistAdapter
         // On submit event handler
         val addWishlistBtn = findViewById<Button>(R.id.addWishlistBtn)
+
         addWishlistBtn.setOnClickListener {
-            
+            val nameEt = findViewById<EditText>(R.id.nameEditText)
+            val priceEt = findViewById<EditText>(R.id.priceEditText)
+            val urlEt = findViewById<EditText>(R.id.urlEditText)
+            val name = nameEt.text.toString()
+            val price = priceEt.text.toString()
+            val url = urlEt.text.toString()
+
+            if (name.isNotEmpty() && price.isNotEmpty() && url.isNotEmpty()) {
+                // Create new wishlist item
+                val wishlist = Wishlist(name, price, url)
+                wishlistAdapter.addWishlist(wishlist)
+            }
+            // Clear Input Fields
+            nameEt.text.clear()
+            priceEt.text.clear()
+            urlEt.text.clear()
         }
     }
 }
--- app/src/main/java/com/example/wishlist/Wishlist.kt
+++ app/src/main/java/com/example/wishlist/Wishlist.kt
@@ -3,6 +3,6 @@ package com.example.wishlist
 // information contained in a wishlist item
 class Wishlist (
     val name: String,
-    val price: Float,
+    val price: String,
     val url: String) {
 }
